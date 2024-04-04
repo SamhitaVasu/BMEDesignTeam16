d77faea594d069aa67e1298fa7015e7ddfb91437
SamhitaVasu <samhita.vasu@gmail.com>
2024-04-04 11:16:44 -0400

    added API get calls for image generation and syntactic part id methods

--- Backend/app.py
+++ Backend/app.py
@@ -2,11 +2,29 @@ from flask import Flask, request, jsonify
 from utils import processing_function, get_chat_response, image_generating_function_quantity, show_image, image_generating_function_object, image_generating_function_action, identify_parts
 app = Flask(__name__)
 
-@app.route('/process', methods=['POST'])
-def process_input():
+@app.route("/parts", methods=["GET"])
+def id_parts():
     data = request.json
-    processed_data = image_generating_function_object(data['input'])
-    return jsonify({"response": processed_data})
+    parts = identify_parts(data['input'])
+    return jsonify({"response": parts})
+
+@app.route("/actionImage", methods=["GET"])
+def gen_action():
+    data = request.json
+    action_image = image_generating_function_action(data['input'])
+    return jsonify({"response": action_image})
+
+@app.route('/objectImage', methods=['GET'])
+def gen_object():
+    data = request.json
+    object_image = image_generating_function_object(data['input'])
+    return jsonify({"response": object_image})
+
+@app.route('/quantImage', methods=['GET'])
+def gen_quant():
+    data = request.json
+    quant_image = image_generating_function_quantity(data['input'])
+    return jsonify({"response": quant_image})
 
 if __name__ == '__main__':
     app.run(debug=True)
